2d1e6e0e8173a87dfe8f8a933ac858dcec797d83
ManhTien <lemanhtien.bka@gmail.com>
2017-06-18 08:10:19 +0700

    Add method to passing array of vocabs from react to native

---
+++ ios/ObjectRecognitionBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjectRecognitionBridge.h
+//  EnglishGo
+//
+//  Created by Tien Le on 6/17/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectRecognitionBridge : NSObject
+
+@end
---
+++ ios/ObjectRecognitionBridge.m
@@ -0,0 +1,13 @@
+//
+//  ObjectRecognitionBridge.m
+//  EnglishGo
+//
+//  Created by Tien Le on 6/17/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "ObjectRecognitionBridge.h"
+
+@implementation ObjectRecognitionBridge
+
+@end
