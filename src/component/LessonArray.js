ff168fa97bd38eb290c866b735d49a0c71737b43
vjtc0n <vjtc0n12345@gmail.com>
2017-06-19 18:57:32 +0700

    Adding array lesson

--- src/component/LessonArray.js
+++ src/component/LessonArray.js
@@ -34,14 +34,126 @@ export default class LessonArray extends Component {
     super(props)
     this.state = {
       lesson2: '',
-      lesson3: ''
+      lesson3: '',
+      lesson1: '',
+      arrayLesson: []
     }
+  }
+  
+  covertLesson(lesson) {
+    let lessonTitle = null
+    switch (lesson) {
+      case '1':
+        lessonTitle = 'air plane'
+        break
+      case '2':
+        lessonTitle = 'boat'
+        break
+      case '3':
+        lessonTitle = 'bicycle'
+        break
+      case '4':
+        lessonTitle = 'bear'
+        break
+      case '5':
+        lessonTitle = 'bird'
+        break
+      case '6':
+        lessonTitle = 'dog'
+        break
+      case '7':
+        lessonTitle = 'bus'
+        break
+      case '8':
+        lessonTitle = 'car'
+        break
+      case '9':
+        lessonTitle = 'motorcycle'
+        break
+      case '10':
+        lessonTitle = 'cat'
+        break
+      case '11':
+        lessonTitle = 'sheep'
+        break
+      case '12':
+        lessonTitle = 'zebra'
+        break
+      case '13':
+        lessonTitle = 'boat'
+        break
+      case '14':
+        lessonTitle = 'bicycle'
+        break
+      case '15':
+        lessonTitle = 'bus'
+        break
+      case '16':
+        lessonTitle = 'elephant'
+        break
+      case '17':
+        lessonTitle = 'giraffe'
+        break
+      case '18':
+        lessonTitle = 'horse'
+        break
+      case '19':
+        lessonTitle = 'traffic light'
+        break
+      case '20':
+        lessonTitle = 'stop sign'
+        break
+      case '21':
+        lessonTitle = 'parking meter'
+        break
+      case '22':
+        lessonTitle = 'bed'
+        break
+      case '23':
+        lessonTitle = 'sink'
+        break
+      case '24':
+        lessonTitle = 'potted plant'
+        break
+      case '25':
+        lessonTitle = 'chair'
+        break
+      case '26':
+        lessonTitle = 'couch'
+        break
+      case '27':
+        lessonTitle = 'bench'
+        break
+      case '28':
+        lessonTitle = 'apple'
+        break
+      case '29':
+        lessonTitle = 'orange'
+        break
+      case '30':
+        lessonTitle = 'banana'
+    }
+    
+    return lessonTitle
   }
   
   componentWillMount() {
     this.setState({
       lesson2: (Number(this.props.navigation.state.params.lesson) + 1).toString(),
-      lesson3: (Number(this.props.navigation.state.params.lesson) + 2).toString()
+      lesson3: (Number(this.props.navigation.state.params.lesson) + 2).toString(),
+      lesson1: (Number(this.props.navigation.state.params.lesson)).toString()
+    }, () => {
+      let lesson1 = this.covertLesson(this.state.lesson1)
+      let lesson2 = this.covertLesson(this.state.lesson2)
+      let lesson3 = this.covertLesson(this.state.lesson3)
+      let arrayLesson = []
+      arrayLesson.push(lesson1)
+      arrayLesson.push(lesson2)
+      arrayLesson.push(lesson3)
+      console.log(arrayLesson)
+      this.setState({
+        arrayLesson: arrayLesson
+      })
     })
   }
   
